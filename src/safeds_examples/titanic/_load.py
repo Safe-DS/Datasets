44822d4154e34a0bb787312c755c4f3ad236c358
Lars Reimann <mail@larsreimann.com>
2023-03-07 17:35:47 +0100

    docs: add docstring

--- src/safeds_examples/titanic/_load.py
+++ src/safeds_examples/titanic/_load.py
@@ -6,4 +6,13 @@ _path = os.path.join(os.path.dirname(__file__), "data", "titanic.csv")
 
 
 def load_titanic() -> Table:
+    """
+    Loads the Titanic dataset.
+
+    Returns
+    -------
+    Table
+        The Titanic dataset.
+    """
+
     return Table.from_csv(_path)
