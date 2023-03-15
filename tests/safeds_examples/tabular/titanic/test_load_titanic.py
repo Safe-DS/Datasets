ca691dd79a97daf492d3ec693df0a594a0cc9433
lars-reimann <lars-reimann@users.noreply.github.com>
2023-03-15 10:23:44 +0000

    style: apply automated linter fixes

--- tests/safeds_examples/tabular/titanic/test_load_titanic.py
+++ tests/safeds_examples/tabular/titanic/test_load_titanic.py
@@ -1,7 +1,11 @@
 import pytest
 from safeds.data.tabular import Table
-from safeds.data.tabular.typing import TableSchema, FloatColumnType, StringColumnType, IntColumnType
-
+from safeds.data.tabular.typing import (
+    FloatColumnType,
+    IntColumnType,
+    StringColumnType,
+    TableSchema,
+)
 from safeds_examples.tabular import load_titanic
 
 
@@ -22,21 +26,21 @@ class TestLoadTitanic:
     def test_schema(self, titanic: Table) -> None:
         assert titanic.schema == TableSchema(
             {
-                'Age': FloatColumnType(),
-                'Cabin Number': StringColumnType(),
-                'Fare': FloatColumnType(),
-                'Name': StringColumnType(),
-                'Number of Parents or Children Aboard': IntColumnType(),
-                'Number of Siblings or Spouses Aboard': IntColumnType(),
-                'Port of Embarkation': StringColumnType(),
-                'Sex': StringColumnType(),
-                'Survived': IntColumnType(),
-                'Ticket Number': StringColumnType(),
-                'Travel Class': IntColumnType()
+                "Age": FloatColumnType(),
+                "Cabin Number": StringColumnType(),
+                "Fare": FloatColumnType(),
+                "Name": StringColumnType(),
+                "Number of Parents or Children Aboard": IntColumnType(),
+                "Number of Siblings or Spouses Aboard": IntColumnType(),
+                "Port of Embarkation": StringColumnType(),
+                "Sex": StringColumnType(),
+                "Survived": IntColumnType(),
+                "Ticket Number": StringColumnType(),
+                "Travel Class": IntColumnType(),
             }
         )
 
     def test_columns_with_missing_values(self, titanic: Table) -> None:
         actual_column_names = {column.name for column in titanic.list_columns_with_missing_values()}
 
-        assert actual_column_names == {'Age', 'Port of Embarkation', 'Fare', 'Cabin Number'}
+        assert actual_column_names == {"Age", "Port of Embarkation", "Fare", "Cabin Number"}
