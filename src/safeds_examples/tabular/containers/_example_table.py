356e3a492f4df42748a8dbb31c36423b601f7463
Lars Reimann <mail@larsreimann.com>
2023-05-11 22:39:15 +0200

    feat: compatibility with `safe-ds` v0.12.0 (#63)
    ### Summary of Changes
    
    Integrate API changes of [`safe-ds`
    v0.12.0](https://github.com/Safe-DS/Stdlib/releases/tag/v0.12.0).
    
    ---------
    
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>

--- src/safeds_examples/tabular/containers/_example_table.py
+++ src/safeds_examples/tabular/containers/_example_table.py
@@ -1,5 +1,5 @@
 from safeds.data.tabular.containers import Table
-from safeds.data.tabular.exceptions import UnknownColumnNameError
+from safeds.exceptions import UnknownColumnNameError
 
 
 class ExampleTable(Table):
@@ -19,13 +19,15 @@ class ExampleTable(Table):
         If a column name in `descriptions` does not exist in `table`.
     """
 
+    # noinspection PyMissingConstructor
     def __init__(self, table: Table, column_descriptions: dict[str, str]) -> None:
         # Check that all column names in `descriptions` exist in `table`
         invalid_column_names = set(column_descriptions.keys()) - set(table.column_names)
         if invalid_column_names:
             raise UnknownColumnNameError(list(invalid_column_names))
 
-        super().__init__(table._data, table.schema)
+        self._data = table._data
+        self._schema = table.schema
         self._descriptions = column_descriptions
 
     @property
@@ -36,10 +38,10 @@ class ExampleTable(Table):
         The name is stored in a column called `"Name"` and the description in a column called `"Description"`.
         """
         return Table(
-            [
-                {"Name": column_name, "Description": self.get_column_description(column_name)}
-                for column_name in self.column_names
-            ],
+            {
+                "Name": self.column_names,
+                "Description": [self.get_column_description(column_name) for column_name in self.column_names],
+            },
         )
 
     def get_column_description(self, column_name: str) -> str:
